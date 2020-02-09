ec721eeea0c3e2181cbe876630427c4c43f29c3e
lawrend <douglas_lawrence@mac.com>
2020-02-08 21:00:17 -0800

    cleaned up layout and added time lived and remaining in hours and seconds view

--- projectapp/scripts.js
+++ projectapp/scripts.js
@@ -5,7 +5,16 @@ const lifespan = 28800;
 // 78 years 310 days;
 
 
-function dummy() {
+secondsConverter = (secs) => {
+  const minutes = Math.floor(secs/60);
+  const remainingSeconds = secs % 60;
+  const hours = Math.floor(minutes/60);
+  const remainingMinutes = minutes % 60;
+  return `${hours} hours, ${remainingMinutes} minutes, ${remainingSeconds} seconds`
+
+}
+
+dummy = () => {
   // displayDiv.innerText = "dummy";
   let nowtimes = moment().format("h:mm:ss a");
   document.getElementById("today").innerText = `${nowtimes}`;
@@ -13,18 +22,18 @@ function dummy() {
 
 function calculateTime () {
   const bday = document.getElementById("name").value
-  const bdayMoment = moment(bday, "YYYYMMDD");
+  const bdayMoment = moment(bday, "MM/DD/YYYY");
 
   let daysAgo = bdayMoment.diff(moment(), 'days')
   let borntime = moment().add(daysAgo, 'seconds').format('h:mm:ss a')
 
   const remainingTime = 28800 + daysAgo;
   let diedtime = moment().add(remainingTime, 'seconds').format("h:mm:ss a")
-  document.getElementById("born").innerText = `${borntime}`;
-  document.getElementById("died").innerText = `${diedtime}`;
-  document.getElementById("timeSince").innerText = `${daysAgo * -1} days ago`;
+  document.getElementById("born").innerText = `${borntime}\n  ${secondsConverter(daysAgo * -1)} lived`;
+  document.getElementById("died").innerText = `${diedtime}\n${secondsConverter(remainingTime)} remain`;
+  document.getElementById("timeSince").innerText = `Born ${daysAgo * -1} days ago`;
   document.getElementById("timeNow").innerText = `now`;
-  document.getElementById("timeToGo").innerText = `${remainingTime} days to go`;
+  document.getElementById("timeToGo").innerText = `${remainingTime} days remain`;
 }
 
 // displayDiv.innerText = `${displayData}`
