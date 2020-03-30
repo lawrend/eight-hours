041c5b1047fe5ccb1c181ae712f9319d0498a68e
lawrend <douglas_lawrence@mac.com>
2020-03-29 18:09:13 -0700

    Updates

---
+++ precache-manifest.6fbaad756d596bf0ea8235a09c864f65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc4245eb338192cbc2c4548abbf0e8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c033677676cdec0c139c",
+    "url": "/static/css/main.1a126dce.chunk.css"
+  },
+  {
+    "revision": "71b4d17e19973ec754e4",
+    "url": "/static/js/2.04a0c6a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.04a0c6a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c033677676cdec0c139c",
+    "url": "/static/js/main.7b192e52.chunk.js"
+  },
+  {
+    "revision": "9d38e79b12a30cbdc5da",
+    "url": "/static/js/runtime-main.c622d288.js"
+  }
+]);
